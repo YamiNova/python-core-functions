5b8768ebf5e38f45fe35d9ab7770a6fa01c39cdf
xhy <hangyu_xia@outlook.com>
2023-07-23 21:17:25 +0800

    add handbook

---
+++ module_regex/handbook.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+
+
+class Handbook(object):
+    def __init__(self):
+        self.__content = None
+
+    def read(self):
+        pass
+
+    def display(self):
+        pass
+
+    def _check(self):
+        pass
